d65d528dca6c722b43596948415da19968ba38ae
db-one <20243226+kang-mk@users.noreply.github.com>
2020-11-12 20:56:11 +0800

    禁用argon主题设置

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -147,7 +147,7 @@ CONFIG_PACKAGE_luci-app-eqos=y #IP限速
 # CONFIG_PACKAGE_luci-app-smartdns=y #smartdns服务器
 # CONFIG_PACKAGE_luci-app-adguardhome=y #ADguardhome
 CONFIG_PACKAGE_luci-app-poweroff=y #关机（增加关机功能）
-CONFIG_PACKAGE_luci-app-argon-config=y #argon主题设置
+# CONFIG_PACKAGE_luci-app-argon-config=y #argon主题设置
 EOF
 
 # ShadowsocksR插件:
