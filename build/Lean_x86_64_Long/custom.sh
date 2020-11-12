f00ef8faf8c2945fec31350b509407d2855ee498
db-one <20243226+kang-mk@users.noreply.github.com>
2020-11-12 12:02:31 +0800

    添加argon主题设置

--- build/Lean_x86_64_Long/custom.sh
+++ build/Lean_x86_64_Long/custom.sh
@@ -22,8 +22,9 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/uci-app-po
 ./scripts/feeds clean
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-# 替换更新默认argon主题
+# 替换更新默认argon主题并添加argon主题设置
 rm -rf package/lean/luci-theme-argon && git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon -b 18.06
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 替换更新passwall和ssrplus+
 rm -rf package/openwrt-packages/luci-app-passwall && svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
