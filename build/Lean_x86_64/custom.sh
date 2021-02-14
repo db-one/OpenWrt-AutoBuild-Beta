2bffbd57e5c840eee446ead742936f57edb78156
db-one <20243226+kang-mk@users.noreply.github.com>
2021-02-14 23:24:32 +0800

    Update

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -155,7 +155,7 @@ CONFIG_PACKAGE_naiveproxy=y
 CONFIG_PACKAGE_kcptun-client=y
 CONFIG_PACKAGE_chinadns-ng=y
 CONFIG_PACKAGE_brook=y
-CONFIG_PACKAGE_v2ray-plugin=n
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=n
 EOF
 
 # 常用LuCI插件:
